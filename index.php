29a0c2fda35b141c87b5c950549e0d6551521d34
MantasTa <125612571+MantasTa@users.noreply.github.com>
2023-02-17 03:18:01 +0200

    s

--- index.php
+++ index.php
@@ -1,12 +1,29 @@
 <?php
-
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
 
 require __DIR__ . '\vendor\autoload.php';
 
 $app = new \Slim\App;
 
-$app-> get('/', function(){
-echo "Helo world";
+
+
+$app->get('/{page}', function (Request $request, Response $response, array $args) {
+    $page = $args['page'];
+
+    switch ($page) {
+      case 'pav':
+        require_once('body/pavadinimai.php');
+        break;
+
+      default:
+        require_once('body/pavadinimai.php');
+        break;
+    }
+
+
+
 });
+
 
 $app->run();
